fe0574758c07c342203f1dc8a5fecf899eadb2ad
Matthew Greco <mgreco2112@tutanota.com>
2021-10-26 21:23:48 -0400

    finished requirements

--- src/main/java/com/careerdevs/employees/Employee.java
+++ src/main/java/com/careerdevs/employees/Employee.java
@@ -9,4 +9,12 @@ public class Employee {
         this.id = id;
         this.name = name;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/careerdevs/employees/EmployeeController.java
+++ src/main/java/com/careerdevs/employees/EmployeeController.java
@@ -1,4 +1,34 @@
 package com.careerdevs.employees;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class EmployeeController {
+    private final AtomicLong id = new AtomicLong();
+    private final long idHeader = 2110;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to the Employee Database";
+    }
+
+    @GetMapping("/demoEmployee")
+    public Employee employeeLanding() {
+        return new Employee(idHeader - 1, "Smith, John");
+    }
+
+    @GetMapping("/selectEmployee")
+    public Employee employeeSelection(@RequestParam(value="name", defaultValue = "Null, Null") String name) {
+        return new Employee(idHeader + id.incrementAndGet(), name);
+    }
+
+    @GetMapping("/select/{name}")
+    public Employee selectEmployee(@PathVariable String name) {
+        return new Employee(idHeader + id.incrementAndGet(), name);
+    }
 }
